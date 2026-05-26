fff5833c13a1525c72f84611dc3f604c318e6a17
laibafatima1325 <laibafatima1325@gmail.com>
2026-05-26 17:44:58 +0500

    Added addExp function

--- main.cpp
+++ main.cpp
@@ -2,15 +2,18 @@
 #include <fstream>
 #include <string>
 using namespace std;
-void storeExp();
+void storeExp(string category, double amount, string note);
 void readExp();
 void addExp();
 void viewExp();
 void totalExp();
 void categoryWiseTotal();
+void storeDate(string date);
 
-float amount;
+double amount;
 string category;
+string note;
+string date;
 const int SIZE = 100;
 
 int main()
@@ -25,6 +28,13 @@ int main()
         cout<<" 5. Exit" <<endl;
         cout<<"Please enter your choice(1-5): ";
         cin>> choice;
+        if(cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invalid input! Please enter only numbers from 1 to 5: ";
+            cin>> choice;
+        }
         if(choice < 1 || choice > 5)
         {
             cout<<"Invalid choice! Please enter a number between 1 and 5: ";
@@ -34,33 +44,135 @@ int main()
         {
             addExp();
         }
-        else if(choice == 2)
+       // else if(choice == 2)
         {
-            viewExp();
+       //     viewExp();
         }
-        else if (choice == 3)
+       // else if (choice == 3)
         {
-            categoryWiseTotal();
+       //     categoryWiseTotal();
         }
-        else if (choice == 4)
+       // else if (choice == 4)
         {
-            totalExp();
+       //     totalExp();
         }
     }while(choice != 5);
     return 0;
 }
 void addExp()
 {
-    int category;
-    cout<<"------CATEGORIES------ " <<endl;
-    cout<<"1. Food "<<endl;
-    cout<<"2. Transport" <<endl;
-    cout<<"3. Shopping"<<endl;
-    cout<<"4. Bills" <<endl;
-    cout<<"5. Health"<<endl;
-    cout<<"6. Education" <<endl;
-    cout<<"7. Entertainment" <<endl;
-    cout<<"8. Other"<<endl;
-    cout<<"Please select a category for which you want to add an expense: " ;
-    cin>> category;
+    int choice, option;
+    cout<<"Enter date(DD-MM-YYYY): ";
+    cin.ignore(1000, '\n');
+    getline(cin, date);
+    storeDate(date);
+    do{
+        cout<<"\n------CATEGORIES------ " <<endl;
+        cout<<"1. Food "<<endl;
+        cout<<"2. Transport" <<endl;
+        cout<<"3. Shopping"<<endl;
+        cout<<"4. Bills" <<endl;
+        cout<<"5. Health"<<endl;
+        cout<<"6. Education" <<endl;
+        cout<<"7. Entertainment" <<endl;
+        cout<<"8. Other"<<endl;
+        cout<<"9. Exit"<<endl;
+        cout<<"Please select a category for which you want to add an expense: " ;
+        cin>> choice;
+        if(cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invalid input! Please enter only numbers(1-9): ";
+            cin>> choice;
+        }
+        if(choice < 1 || choice > 9)
+        {
+            cout <<"Invalid! Please enter numbers between 1 and 9: ";
+            cin>> choice;
+        }
+        if(choice == 9)
+        {
+            return;
+        }
+        if(choice == 1)
+            category = "Food";
+        else if(choice == 2)
+            category = "Transport";
+        else if(choice == 3)
+            category = "Shopping";
+        else if(choice == 4)
+            category = "Bills";
+        else if(choice == 5)
+            category = "Health";
+        else if(choice == 6)
+            category = "Education";
+        else if(choice == 7)
+            category = "Entertainment";
+        else if(choice == 8)
+        {
+            cin.ignore(1000, '\n');
+            cout << "Enter category name: ";
+            getline(cin, category);
+        }
+        do{
+            cout << "Please enter amount: ";
+            cin >> amount;
+
+            if(cin.fail())
+            {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cout << "Invalid input! Enter numbers only"<<endl;
+                amount = -1;
+            }
+            else if(amount <= 0)
+            {
+                cout << "Amount must be greater than 0"<<endl;
+            }
+
+        } while(amount <= 0);
+
+        cin.ignore(1000, '\n');
+        cout << "Enter a note/description of where you have spent your money: ";
+        getline(cin, note);
+
+        storeExp(category, amount, note);
+
+        cout<<"/nExpense added successfully!"<<endl;
+        cout<<"1. Add another expense"<<endl;
+        cout<<"2. Finish day"<<endl;
+        cout<<"Please select an option: ";
+        cin>> option;
+        cin.ignore(1000, '\n');
+        break;
+    }while(option != 2);
+}
+void storeExp(string category, double amount, string note)
+{
+    ofstream fin("expenses.txt", ios::app);
+        if(!fin.fail())
+        {
+            fin << "Category: " <<category << endl;
+            fin << "Amount: " <<amount << endl;
+            fin << "Note: " <<note << endl;
+            fin.close();
+        }
+        else
+        {
+            cout << "\nError opening file!";
+        }
+}
+void storeDate(string date)
+{
+    ofstream file("expenses.txt", ios::app);
+    if(!file.fail())
+    {
+        file<<"===============\n";
+        file<<"Date: "<<date <<endl;
+        file.close();
+    }
+    else{
+        cout<<"Error opening file!";
+    }
 }
